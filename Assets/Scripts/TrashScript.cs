8f3fa6410c287cb8bc943da2fe4d67e78878291b
bigwowik-PC <bigwowik@gmail.com>
2020-10-03 14:11:27 +0300

    1

---
+++ Assets/Scripts/TrashScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+    public enum ObjectType
+    { Object1, Object2, Object3 };
+    public ObjectType myObjectType = ObjectType.WhiteSkull;
+}
