1d97dfb5a78ee83a0b0759ebac034e04e0f7084c
bigwowik-PC <bigwowik@gmail.com>
2020-10-05 12:03:55 +0300

    1 лвл доделал и меню лвл 0

---
+++ Assets/Scripts/MenuControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuControllerScript : MonoBehaviour
+{
+    private GameObject spawn;
+    public GameObject bg;
+    public GameObject lastBackGround;
+
+    public void SpawnBackGround()
+    {
+        print("spawn");
+        GameObject newBg = Instantiate(bg, lastBackGround.transform.Find("SpawnPoint").transform.position, lastBackGround.transform.rotation);
+        lastBackGround = newBg;
+    }
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
