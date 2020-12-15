8be4c57e4302d432ee6bd8b84e39a4a629953cae
maria-PC <bigwowik@gmail.com>
2020-12-15 18:17:10 +0300

    -

---
+++ Assets/Scripts/New/SavePositionOnLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePositionOnLevel : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("PlayerPositionX", transform.position.x);
+        PlayerPrefs.SetFloat("PlayerPositionY", transform.position.y);
+        PlayerPrefs.SetFloat("PlayerPositionZ", transform.position.z);
+    }
+    private void Start()
+    {
+        transform.position = new Vector3(PlayerPrefs.GetFloat("PlayerPositionX"), PlayerPrefs.GetFloat("PlayerPositionY"), PlayerPrefs.GetFloat("PlayerPositionZ"));
+
+    }
+}
